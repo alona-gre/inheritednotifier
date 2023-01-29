1bbec544c6c5f09dc7d8f86c4fba0096b0e8f74e
Alena Grechka <alyonagrechka1@gmail.com>
2023-01-29 21:49:50 +0000

    step1

---
+++ android/app/src/main/kotlin/com/example/inheritednotifier_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inheritednotifier_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class SliderData extends ChangeNotifier {
+  double _value = 0.0;
+  double get value => _value;
+  set value(double newValue) {
+    if (newValue != _value) {
+      _value = newValue;
+      notifyListeners();
+    }
+  }
+}
+
+final sliderData = SliderData();
+
+class SliderInheritedNotifier extends InheritedNotifier<SliderData> {
+  const SliderInheritedNotifier({
+    Key? key,
+    required Widget child,
+    required SliderData sliderData,
+  }) : super(
+          key: key,
+          child: child,
+          notifier: sliderData,
+        );
+
+  static double of(BuildContext context) =>
+      context
+          .dependOnInheritedWidgetOfExactType<SliderInheritedNotifier>()
+          ?.notifier
+          ?.value ??
+      0.0;
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: SliderInheritedNotifier(
+        sliderData: sliderData,
+        child: Builder(
+          builder: (context) {
+            return Column(
+              children: [
+                Slider(
+                  value: SliderInheritedNotifier.of(context),
+                  onChanged: (value) {
+                    sliderData.value = value;
+                  },
+                ),
+                Row(
+                  children: [
+                    Opacity(
+                      opacity: SliderInheritedNotifier.of(context),
+                      child: Container(
+                        color: Colors.yellow,
+                        height: 100,
+                      ),
+                    ),
+                    Opacity(
+                      opacity: SliderInheritedNotifier.of(context),
+                      child: Container(
+                        color: Colors.blue,
+                        height: 100,
+                      ),
+                    ),
+                  ].expandEqually().toList(),
+                )
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+extension ExpandEqually on Iterable<Widget> {
+  Iterable<Widget> expandEqually() => map(
+        (w) => Expanded(child: w),
+      );
+}
